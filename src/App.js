a3a5ed538b9916c9c2a39c457faa63ee93339a71
yuvrajpanwar <yogipanwar173@gmail.com>
2024-04-19 01:08:07 +0530

    initial commit3

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import logo from './logo.svg';
+import logo from './me.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          First React App by Yuvraj
-        </p>
+      <a
+          className="App-link"
+          href ="https://yuvrajpanwar.github.io/portfolio"
+          target='_blank'
+          rel="noreferrer"
+        >
+        <img src={logo} className="App-logo" alt="logo" /></a>
         <a
           className="App-link"
           href ="https://yuvrajpanwar.github.io/portfolio"
           target='_blank'
           rel="noreferrer"
         >
-          Checkout my portfolio
+          --Checkout my portfolio--
         </a>
       </header>
     </div>
